9750983cdf6c3b1d529a73f84e2d6ae90fdab420
perrot <linca30mus@gmail.com>
2021-11-29 22:14:09 +0800

    * 新增 active連結到導覽列。 * 將footer定在頁底。

--- index.php
+++ index.php
@@ -7,12 +7,47 @@
     <title>許志浩的作品集</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="shortcut icon" href="./images/favicon.png" type="image/png">
+    <style>
+        * {
+            /* border: 2px solid black; */
+            margin: 0;
+            padding: 0;
+        }
+        
+        html,
+        body {
+            height: 100%;
+        }
+        
+        .wrap {
+            min-height: 100%;
+        }
+        
+        .main {
+            /* 有需要卷軸自動出現 */
+            overflow: auto;
+            padding-bottom: 150px;
+            /* this needs to be bigger than footer height*/
+        }
+        
+        .footer {
+            /* position: relative; */
+            /* 負值往上長 */
+            margin-top: -150px;
+            height: 150px;
+            /* clear: both; */
+        }
+    </style>
 </head>
 <body>
+  <div class="wrap">
+    <div class="main">
+
+    
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
   <ul class="navbar-nav">
   <li class="nav-item">
-      <a class="nav-link" href="#">ALL</a>
+      <a class="nav-link active" href="#">ALL</a>
     </li>
     <?php
 $collectionDir = './collection';
@@ -66,9 +101,17 @@ foreach ($rootDir as $category) {
 <!--
     <div class="col-4 p-2 border"><img class="img-fluid" src="./Projects/calendar/screenshot.png" alt=""></div>
     <div class="col-4 p-2 border">Flex item 1</div>-->
-</div>
+  </div>
 </div>
 
+</div>
+  </div>
+<!-- Copyright start -->
+<footer class="jumbotron footer text-center py-3">© 2020 Copyright:
+    <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+  </footer>
+
+  <!-- Copyright end-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
@@ -76,6 +119,8 @@ foreach ($rootDir as $category) {
 $(document).ready(function(){
 $(".nav-link").on("click", function() {
     var value =$(this).text().toLowerCase();
+    $(".nav-link").removeClass("active");
+    $(this).addClass("active");
     if(value=="all") value="";
     $("#homework div").filter(function() {
       $(this).toggle($(this).attr('onclick').toLowerCase().indexOf(value) > -1)
